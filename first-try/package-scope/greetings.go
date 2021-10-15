b8456b28b6a133dbbf72b55fb7700d7241651851
Eddie Cheng <schaffenneu47@gmail.com>
2021-10-15 22:18:09 +0800

    fist see package scope

---
+++ first-try/package-scope/greetings.go
@@ -0,0 +1,10 @@
+package main
+
+// fmt: for formatting and printing
+import "fmt"
+
+var points = []int{1, 2, 3, 4, 5, 6, 7, 4324, 444, 22344}
+
+func sayKnife(n string) {
+	fmt.Println("hellow", n)
+}
---
+++ first-try/package-scope/main.go
@@ -0,0 +1,14 @@
+package main
+
+// fmt: for formatting and printing
+import "fmt"
+
+// main(): the entry point of the application
+func main() {
+	sayKnife("banana")
+
+	for _, v := range points {
+		fmt.Println(v)
+	}
+
+}
