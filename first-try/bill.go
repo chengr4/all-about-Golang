2b478412fc57f6d8c17b22cc0d2a9f2da94d720a
Eddie Cheng <schaffenneu47@gmail.com>
2021-10-27 16:51:16 +0800

    first see saving files

--- first-try/bill.go
+++ first-try/bill.go
@@ -1,7 +1,7 @@
 package main
 
 /*
-this file includes concepts: input, switch
+this file includes concepts: input, switch, parsing floats, saving files
 */
 
 // fmt: for formatting and printing
@@ -9,6 +9,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -50,6 +51,49 @@ func createBill() bill {
 	return b
 }
 
+// add item to bill
+func (b *bill) addItem(name string, price float64) {
+	b.items[name] = price
+}
+
+// update tip
+func (b *bill) updateTip(tip float64) {
+	// here will dereference itself automatically
+	b.tip = tip
+}
+
+// format the bill
+func (b *bill) format() string {
+	fs := "Bill brealdown: \n"
+	var total float64 = 0
+
+	// list items
+	for k, v := range b.items {
+		fs += fmt.Sprintf("%v ... $%v \n", k+":", v)
+		total += v
+	}
+
+	fs += fmt.Sprintf("%v ... $%v\n", "tip:", b.tip)
+
+	// total
+	fs += fmt.Sprintf("%v ... $%0.2f\n", "total:", total+b.tip)
+
+	return fs
+}
+
+// save bill
+func (b *bill) saveBill() {
+	data := []byte(b.format())
+
+	// 0644: permission
+	err := os.WriteFile("bills/"+b.name+".txt", data, 0644)
+	if err != nil {
+		// stop the flow
+		panic(err)
+	}
+	fmt.Println("Bill was saved")
+}
+
 func promptOptions(b bill) {
 	reader := bufio.NewReader(os.Stdin)
 	option, _ := getInput("Choose an option (a: add a item, s: save the bill, t: add the tip): ", reader)
@@ -61,14 +105,34 @@ func promptOptions(b bill) {
 		name, _ := getInput("Item name: ", reader)
 		price, _ := getInput("Item price: ", reader)
 
+		p, err := strconv.ParseFloat(price, 64)
+		if err != nil {
+			fmt.Println("THe price must be a number")
+			promptOptions(b)
+		}
+		b.addItem(name, p)
+
 		fmt.Println("name: ", name, ", price: ", price)
+		promptOptions(b)
+
 	case "t":
 		fmt.Println("you chose t")
-		tip, _ := getInput("Enter tip amount ($)", reader)
+		tip, _ := getInput("Enter tip amount ($): ", reader)
+		t, err := strconv.ParseFloat(tip, 64)
+		if err != nil {
+			fmt.Println("THe price must be a number")
+			promptOptions(b)
+		}
+		b.updateTip(t)
 
-		fmt.Println("tip: ", tip)
+		fmt.Println("tip added - ", tip)
+		promptOptions(b)
+
 	case "s":
-		fmt.Println("you chose s")
+		fmt.Println("you chose s to save the bill")
+		fmt.Println(b.format())
+		b.saveBill()
+
 	default:
 		fmt.Println("that was not a vaild option...")
 		promptOptions(b)
