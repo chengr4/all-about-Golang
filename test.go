c23254d35ca4b164f912ac5f0c995a0343fa9529
Eddie Cheng <schaffenneu47@gmail.com>
2022-02-04 23:34:00 +0800

    add test file

---
+++ test.go
@@ -0,0 +1,20 @@
+package main
+
+// fmt: for formatting and printing
+import "fmt"
+
+type score struct {
+	x int32
+	y int64
+}
+
+func changeXWithPointer(pt *score) {
+
+}
+
+// main(): the entry point of the application
+func main() {
+	p1 := &score{x: 3}
+	fmt.Println(p1)
+	changeXWithPointer(p1)
+}
